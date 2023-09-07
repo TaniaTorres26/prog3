1e84359824b1306ab7d0fd7cd8f1b4f11468b832
TaniaTorres26 <141367224+TaniaTorres26@users.noreply.github.com>
2023-09-06 23:24:39 -0500

    Avance 2 ejercicio 1 lab 2

--- labs/2/OperacionesMatrices.java
+++ labs/2/OperacionesMatrices.java
@@ -20,6 +20,31 @@ public class OperacionesMatrices {
             }
         }
 
+        while (true) {
+            System.out.println("\nSeleccione una operación:");
+            System.out.println("1. Suma de matrices");
+            System.out.println("2. Producto de matrices");
+            System.out.println("3. Producto de un escalar con una matriz");
+            System.out.println("4. Traspuesta de una matriz");
+            System.out.println("5. Salir");
 
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese la segunda matriz para la suma:");
+                    double[][] matrizSuma = leerMatriz(filas, columnas);
+                    double[][] resultadoSuma = sumarMatrices(matriz, matrizSuma);
+                    imprimirMatriz(resultadoSuma);
+                    break;
+                case 2:
+                    System.out.println("Ingrese la segunda matriz para el producto:");
+                    int filas2 = columnas;
+                    System.out.print("Ingrese el número de columnas de la segunda matriz: ");
+                    int columnas2 = scanner.nextInt();
+                    double[][] matrizProducto = leerMatriz(filas2, columnas2);
+            }
+}
+   
 }
 }
