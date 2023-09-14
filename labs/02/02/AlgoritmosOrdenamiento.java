c4675c2fe7340d51486334b83e42495408e3c7c3
TaniaTorres26 <141367224+TaniaTorres26@users.noreply.github.com>
2023-09-13 22:40:30 -0500

    Add files via upload

---
+++ labs/02/02/AlgoritmosOrdenamiento.java
@@ -0,0 +1,14 @@
+public class AlgoritmosOrdenamiento {
+    public static void burbuja(double[] arr) {
+    }
+
+    public static void insercion(double[] arr) {
+    }
+
+    public static void seleccion(double[] arr) {
+    }
+
+    public static void mergeSort(double[] arr) {
+    }
+}
+
