090da6719bf15551160a5d0ecdccbefbdd116cf1
TaniaTorres26 <141367224+TaniaTorres26@users.noreply.github.com>
2023-09-28 14:07:38 -0500

    Add files via upload

--- labs/04/Ej0402.java
+++ labs/04/Ej0402.java
@@ -1,25 +1,26 @@
-package com.mycompany.ej0401;
+package com.mycompany.ej0402;
 
-/**
- *
- * @author Tania
- */
 import java.util.Timer;
 import java.util.TimerTask;
+
 public class Ej0402 {
 
- public static void main(String[] args){
- TimerTask timertask = new TimerTask() {
- int i=0;
- @Override
- public void run() {
- System.out.println("Hola "+i);
- i++;
- }
- };
+    public static void main(String[] args) {
+        // Creamos una tarea de temporizador que imprimirá un saludo y un contador
+        TimerTask timertask = new TimerTask() {
+            int i = 0;
 
- Timer timer = new Timer();
- timer.scheduleAtFixedRate(timertask, 0, 2000);
- }
+            @Override
+            public void run() {
+                System.out.println("Hola " + i); // Imprimimos un mensaje con un contador
+                i++; // Incrementamos el contador
+            }
+        };
+
+        Timer timer = new Timer(); // Creamos una instancia de Timer para controlar la tarea
+        // Programamos la tarea para que se ejecute cada 2 segundos (2000 milisegundos)
+        // a partir de un retraso inicial de 0 milisegundos
+        timer.scheduleAtFixedRate(timertask, 0, 2000);
+    }
 
 };
