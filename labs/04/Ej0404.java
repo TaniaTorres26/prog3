7b5f9df29a26b8c70b64bff972e93c9a7ac74984
TaniaTorres26 <141367224+TaniaTorres26@users.noreply.github.com>
2023-09-28 14:12:18 -0500

    Add files via upload

--- labs/04/Ej0404.java
+++ labs/04/Ej0404.java
@@ -14,44 +14,43 @@ import java.awt.event.ActionEvent;
 
 public class Ej0404 extends javax.swing.JFrame {
 
-    Timer temporizador;
-    ActionListener al;
+    Timer temporizador; // Declaración de un objeto Timer para controlar el temporizador.
+    ActionListener al; // Declaración de un objeto ActionListener para definir la acción del temporizador.
 
     public Ej0404() {
-        initComponents();
+        initComponents(); // Inicializa los componentes de la interfaz gráfica.
 
         al = new ActionListener() {
-            int i = 0;
+            int i = 0; // Inicializamos una variable 'i' para el movimiento vertical.
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Calculamos la posición horizontal 'x' utilizando una función sinusoidal.
                 double x = 50 + 10 * Math.sin(2 * Math.PI / 10 * (double) i);
-                jRadioButton1.setLocation((int) x, i);
-                i++;
+                jRadioButton1.setLocation((int) x, i); 
+                i++; // Incrementamos 'i' para cambiar la posición vertical en cada ejecución.
             }
         };
 
-        temporizador = new Timer(100, al); 
+        temporizador = new Timer(100, al); // Creamos un temporizador que ejecutará la acción cada 100 milisegundos.
     }
 
     @SuppressWarnings("unchecked")
     private void initComponents() {
-
+       
     }
 
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
-        if (!temporizador.isRunning()) {
-            temporizador.start();
-            jButton1.setText("Detener");
-        } else {
-            temporizador.stop();
-            jButton1.setText("Iniciar");
+        if (!temporizador.isRunning()) { // Si el temporizador no está en funcionamiento
+            temporizador.start(); // Iniciamos el temporizador.
+            jButton1.setText("Detener"); // Cambiamos el texto del botón a "Detener".
+        } else { // Si el temporizador está en funcionamiento
+            temporizador.stop(); // Detenemos el temporizador.
+            jButton1.setText("Iniciar"); // Cambiamos el texto del botón a "Iniciar".
         }
     }
 
     public static void main(String args[]) {
-
     }
     private javax.swing.JButton jButton1;
     private javax.swing.JRadioButton jRadioButton1;
