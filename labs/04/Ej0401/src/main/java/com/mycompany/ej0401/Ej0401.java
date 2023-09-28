6dccddd99096176be58a59cd82220ad390570ecb
TaniaTorres26 <141367224+TaniaTorres26@users.noreply.github.com>
2023-09-28 12:27:34 -0500

    Add files via upload

---
+++ labs/04/Ej0401/src/main/java/com/mycompany/ej0401/Ej0401.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ej0401;
+
+/**
+ *
+ * @author Estudiante
+ */
+import java.util.Timer;
+import java.util.TimerTask;
+public class Ej0401 {
+ Timer timer;
+ public Ej0401(int segundos) {
+ timer = new Timer();
+ timer.schedule(new Recordatorio(),segundos*1000);//Programa la tarea
+ }
+ class Recordatorio extends TimerTask {
+ public void run() {
+ System.out.println("Tiene un recordatorio!");
+ timer.cancel(); //Termina el hilo del timer
+ }
+ }
+ public static void main(String args[]) {
+ System.out.println("Inicio del programa");
+ new Ej0401(5);
+ }
+}
